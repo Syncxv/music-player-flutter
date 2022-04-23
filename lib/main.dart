edbd93a09a8010646da8b251c41289885acab00a
Syncxv <aruldevarackal1@gmail.com>
2022-04-23 16:38:36 +0530

    init

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:music_player/palette/palette.dart';
+import 'package:music_player/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        primaryColor: Palette.purpleMain,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
---
+++ lib/palette/palette.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static Color purpleMain = const Color.fromRGBO(39, 21, 62, 1);
+}
