50948dcd84f57e15310c68dc17f382e7f3ee4409
Syncxv <aruldevarackal1@gmail.com>
2022-04-24 01:18:36 +0530

    cover :O

--- lib/models/song.dart
+++ lib/models/song.dart
@@ -3,9 +3,11 @@ class Song {
   final String artist;
   final String album;
   final String comment;
+  final String? cover;
   final int year;
 
   Song({
+    this.cover,
     required this.title,
     required this.artist,
     required this.album,
