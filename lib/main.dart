d01d9235d925f28bb6f99996cd6ce8f9a38e9d6d
Syncxv <aruldevarackal1@gmail.com>
2022-04-24 01:26:06 +0530

    oopsie const

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         primaryColor: Palette.purpleMain,
         scaffoldBackgroundColor: Palette.purpleMain,
       ),
-      home: HomeScreen(),
+      home: const HomeScreen(),
     );
   }
 }
