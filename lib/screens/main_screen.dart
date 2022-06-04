12b1dcd9dfda6b66cdfd9723b0a3be7db2048005
Syncxv <aruldevarackal1@gmail.com>
2022-06-04 14:48:55 +0530

    animations are a pain ngl

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -10,22 +10,57 @@ class MainScreen extends StatefulWidget {
   State<MainScreen> createState() => _MainScreenState();
 }
 
-class _MainScreenState extends State<MainScreen> {
+class _MainScreenState extends State<MainScreen>
+    with SingleTickerProviderStateMixin {
   final List<Widget> _screens = [];
-  final List<Text> _text = const [Text("Home"), Text("Playlists")];
   int _selectedIndex = 0;
   bool _isOpened = false;
+  double rotation = 0.0;
+  double offset = 0.0;
+
+  //controllers
+  AnimationController? _controllerA;
+  var scaleAnimation;
 
   @override
   void initState() {
     _screens.add(HomeScreen(onClick: onClick));
+    _controllerA = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 200),
+    );
+    _controllerA!.addListener(() {
+      print(scaleAnimation!.value);
+      setState(() {
+        rotation = -0.15 * scaleAnimation.value;
+        offset = 220.0 * scaleAnimation.value;
+      });
+    });
+    scaleAnimation = Tween(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(CurvedAnimation(
+      parent: _controllerA!,
+      curve: Curves.easeInOut,
+    ));
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controllerA?.dispose();
+    super.dispose();
   }
 
   void onClick() {
     setState(() {
       _isOpened = !_isOpened;
     });
+    if (_controllerA!.isCompleted) {
+      _controllerA!.reverse();
+    } else {
+      _controllerA!.forward(from: 0.0);
+    }
   }
 
   Widget getCurrentScreen() =>
@@ -34,25 +69,45 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     print(_isOpened);
-    return _isOpened
-        ? Stack(
-            children: [
-              const NavBar(),
-              Transform.translate(
-                offset: const Offset(220.0, 0),
-                child: Transform.scale(
-                  scale: 0.95,
-                  child: Transform.rotate(
-                    angle: -0.15,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(18.0),
-                      child: getCurrentScreen(),
-                    ),
-                  ),
-                ),
+    return Stack(
+      children: [
+        const NavBar(),
+        Transform.translate(
+          offset: Offset(offset, 0),
+          child: Transform.scale(
+            scale: 1,
+            child: Transform.rotate(
+              angle: rotation,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(18.0),
+                child: getCurrentScreen(),
               ),
-            ],
-          )
-        : getCurrentScreen();
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class RotateScale extends StatelessWidget {
+  final Widget child;
+  const RotateScale({Key? key, required this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform.translate(
+      offset: const Offset(220.0, 0),
+      child: Transform.scale(
+        scale: 0.95,
+        child: Transform.rotate(
+          angle: -0.15,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(18.0),
+            child: child,
+          ),
+        ),
+      ),
+    );
   }
 }
