30d5ce9056d6022735da142d2c74d50475d0320a
Syncxv <aruldevarackal1@gmail.com>
2022-06-06 13:20:49 +0530

    thingy at the top :P

--- lib/widgets/nav_bar.dart
+++ lib/widgets/nav_bar.dart
@@ -14,63 +14,99 @@ class NavBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Palette.purpleBg,
-      body: Container(
-        margin: const EdgeInsets.only(left: 20, top: 100.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: 80,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(18.0),
-                child: const Image(
-                  image: NetworkImage('https://via.placeholder.com/150'),
-                ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Transform.translate(
+            offset: const Offset(-25, -15),
+            child: Container(
+              height: 100,
+              width: 200,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50),
+                boxShadow: [
+                  BoxShadow(
+                    color:
+                        const Color.fromARGB(255, 255, 255, 255).withAlpha(40),
+                    blurRadius: 100.0,
+                    spreadRadius: 0.0,
+                  ),
+                ],
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20),
-              child: Text(
-                "Hi!",
-                style: TextStyle(
-                  color: Colors.grey.shade400,
-                  fontSize: 16,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                bottom: 40.0,
-              ),
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width * 0.3,
-                child: const Text(
-                  "Wigga Boy",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 25,
+          ),
+          Container(
+            margin: const EdgeInsets.only(left: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: 80,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    boxShadow: [
+                      BoxShadow(
+                        color: const Color.fromARGB(255, 255, 255, 255)
+                            .withAlpha(60),
+                        blurRadius: 30.0,
+                        spreadRadius: 0.0,
+                      ),
+                    ],
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(18.0),
+                    child: const Image(
+                      image: NetworkImage(
+                        'https://cdn.discordapp.com/avatars/549244932213309442/5b3984903ff4d507f93465a1e1d86ec7.webp',
+                      ),
+                    ),
                   ),
                 ),
-              ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20),
+                  child: Text(
+                    "Hi!",
+                    style: TextStyle(
+                      color: Colors.grey.shade400,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    bottom: 40.0,
+                  ),
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.3,
+                    child: const Text(
+                      "Wigga Boy",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25,
+                      ),
+                    ),
+                  ),
+                ),
+                NavItem(
+                  icon: Icons.home,
+                  text: "Home",
+                  onClick: () => setScreen(0),
+                  selected: index == 0,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                NavItem(
+                  icon: Icons.add_road,
+                  text: "Test",
+                  onClick: () => setScreen(1),
+                  selected: index == 1,
+                ),
+              ],
             ),
-            NavItem(
-              icon: Icons.home,
-              text: "Home",
-              onClick: () => setScreen(0),
-              selected: index == 0,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            NavItem(
-              icon: Icons.add_road,
-              text: "Test",
-              onClick: () => setScreen(1),
-              selected: index == 1,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
