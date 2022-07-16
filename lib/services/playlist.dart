56149795cc7768054bd6645632c6ead2e1651f87
Syncxv <aruldevarackal1@gmail.com>
2022-07-17 03:36:56 +0530

    whoops

--- lib/services/playlist.dart
+++ lib/services/playlist.dart
@@ -2,14 +2,14 @@ import 'package:music_player/constants/index.dart';
 import 'package:music_player/models/models.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-Future<Playlist> getAllPlaylists() async {
+Future<List<Playlist>> getAllPlaylists() async {
   //TODO: DO IT WIGGA
   final prefs = await SharedPreferences.getInstance();
   final wigga = await prefs.getString(Constants.playlistKey);
   if (wigga == null) {
     print("woah something went wrong please look at me");
-    return [] as Future<Playlist>;
+    return [];
   }
 
-  return [] as Future<Playlist>;
+  return [];
 }
