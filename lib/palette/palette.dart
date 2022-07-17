bbf2e6731aacb8598aeacc69921d99e3979f7023
Syncxv <aruldevarackal1@gmail.com>
2022-07-17 06:49:11 +0530

    const

--- lib/palette/palette.dart
+++ lib/palette/palette.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 class Palette {
   static Color purpleMain = const Color.fromRGBO(187, 102, 254, 1);
   static Color purpleBg = const Color.fromRGBO(38, 6, 48, 1);
-  static Color textColorWhite = Color.fromARGB(255, 237, 237, 237);
+  static Color textColorWhite = const Color.fromARGB(255, 237, 237, 237);
 }
