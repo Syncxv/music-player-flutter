8085ef2146577b79af09b97b064a39cc369a254a
Syncxv <aruldevarackal1@gmail.com>
2022-07-17 06:49:19 +0530

    ok now time to test

--- lib/widgets/playlist_tab_widget.dart
+++ lib/widgets/playlist_tab_widget.dart
@@ -2,15 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:music_player/constants/index.dart';
 import 'package:music_player/models/models.dart';
 import 'package:music_player/palette/palette.dart';
+import 'package:music_player/services/playlist.dart';
+import 'package:music_player/util/generateId.dart';
 
 class PlaylistTabBodyIDkMan extends StatelessWidget {
   const PlaylistTabBodyIDkMan({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final playlists = getAllPlaylists();
+    print(playlists);
     return Column(
       children: [
         PlaylistWidget(
+          onTap: () {
+            insertPlaylist(
+              Playlist(
+                  dateCreated: DateTime.now(),
+                  id: generateId(),
+                  name: "hi",
+                  songs: []),
+            );
+          },
           boxThingy: Container(
             color: Palette.purpleBg,
             height: 55,
@@ -38,35 +51,42 @@ class PlaylistTabBodyIDkMan extends StatelessWidget {
 class PlaylistWidget extends StatelessWidget {
   final Playlist playlist;
   final Widget boxThingy;
+  final Function onTap;
   const PlaylistWidget(
-      {Key? key, required this.playlist, required this.boxThingy})
+      {Key? key,
+      required this.playlist,
+      required this.boxThingy,
+      required this.onTap})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(
-        vertical: LibraryScreenConstnats.padding - 5.0,
-        horizontal: LibraryScreenConstnats.padding,
-      ),
-      child: Row(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(8.0),
-            child: boxThingy,
-          ),
-          const SizedBox(
-            width: LibraryScreenConstnats.playlistPadding,
-          ),
-          Text(
-            playlist.name,
-            style: TextStyle(
-              color: Palette.textColorWhite,
-              fontWeight: FontWeight.w500,
-              fontSize: 17,
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        padding: const EdgeInsets.symmetric(
+          vertical: LibraryScreenConstnats.padding - 5.0,
+          horizontal: LibraryScreenConstnats.padding,
+        ),
+        child: Row(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(8.0),
+              child: boxThingy,
             ),
-          )
-        ],
+            const SizedBox(
+              width: LibraryScreenConstnats.playlistPadding,
+            ),
+            Text(
+              playlist.name,
+              style: TextStyle(
+                color: Palette.textColorWhite,
+                fontWeight: FontWeight.w500,
+                fontSize: 17,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
